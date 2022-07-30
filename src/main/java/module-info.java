7c0e2f51993a8f62173919bd6e344fbe5247f45e
Quentin Ligier <quentin.ligier@bluewin.ch>
2022-07-30 18:59:14 +0200

    Don't directly import flexmark

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,15 +10,6 @@ module pixel.dng.fixer {
     requires com.sandec.mdfx;
     requires com.github.mjeanroy.exiftool;
     requires java.desktop;
-    requires flexmark;
-    requires flexmark.util.collection;
-    requires flexmark.util.misc;
-    requires flexmark.util.options;
-    requires flexmark.util.sequence;
-    requires flexmark.util.visitor;
-    requires flexmark.ext.attributes;
-    requires flexmark.ext.gfm.tasklist;
-    requires flexmark.ext.tables;
 
     opens ch.qligier.app.pixeldngfixer to javafx.graphics;
     opens ch.qligier.app.pixeldngfixer.gui to javafx.fxml;
